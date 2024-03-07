d7a644b20c8b80a7a559fbc2359d58c1e973f8e7
LuisGilGB <luis.gilgb@gmail.com>
2024-03-07 02:09:01 +0100

    Basic layout improvements in Gen UI Chat

--- app/gen-ui-chat/page.tsx
+++ app/gen-ui-chat/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useState } from 'react'
-import { useActions, useUIState } from 'ai/rsc'
 import { AI } from '@/app/actions'
+import { useActions, useUIState } from 'ai/rsc'
+import { useState } from 'react'
 
 export default function Page() {
   const [inputValue, setInputValue] = useState('')
@@ -10,42 +10,47 @@ export default function Page() {
   const { submitUserMessage } = useActions<typeof AI>()
 
   return (
-    <div>
-      {
-        // View messages in UI state
-        messages.map(message => (
-          <div key={message.id}>{message.display}</div>
-        ))
-      }
+    <div className="flex flex-col w-full max-w-[720px] mx-auto p-12">
+      <div>
+        {
+          // View messages in UI state
+          messages.map(message => (
+            <div key={message.id}>{message.display}</div>
+          ))
+        }
 
-      <form
-        onSubmit={async e => {
-          e.preventDefault()
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
 
-          // Add user message to UI state
-          setMessages(currentMessages => [
-            ...currentMessages,
-            {
-              id: Date.now(),
-              display: <div className="bg-green-300">{inputValue}</div>
-            }
-          ])
+            // Add user message to UI state
+            setMessages(currentMessages => [
+              ...currentMessages,
+              {
+                id: Date.now(),
+                display: <div className="bg-green-300">{inputValue}</div>
+              }
+            ])
 
-          // Submit and get response message
-          const responseMessage = await submitUserMessage(inputValue)
-          setMessages(currentMessages => [...currentMessages, responseMessage])
+            // Submit and get response message
+            const responseMessage = await submitUserMessage(inputValue)
+            setMessages(currentMessages => [
+              ...currentMessages,
+              responseMessage
+            ])
 
-          setInputValue('')
-        }}
-      >
-        <input
-          placeholder="Send a message..."
-          value={inputValue}
-          onChange={event => {
-            setInputValue(event.target.value)
+            setInputValue('')
           }}
-        />
-      </form>
+        >
+          <input
+            placeholder="Send a message..."
+            value={inputValue}
+            onChange={event => {
+              setInputValue(event.target.value)
+            }}
+          />
+        </form>
+      </div>
     </div>
   )
 }
