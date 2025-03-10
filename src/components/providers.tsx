0bb2b85b41d980f97b4d97ffb5da663e407cb0b7
LuisGilGB <luis.gilgb@gmail.com>
2025-03-10 21:55:58 +0100

    Removed unused API routes and fixed build

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
 import { SidebarProvider } from '@/lib/hooks/use-sidebar'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
